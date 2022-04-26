695a686015e9895e2cec780b70ac58926818aa67
Danielly Primo <60229135+dan-primo@users.noreply.github.com>
2022-04-26 00:19:18 -0300

    feat: Initial project

---
+++ Newsletter.Consumer/Program.cs
@@ -0,0 +1,52 @@
+﻿using Confluent.Kafka;
+using Serilog;
+using System;
+
+namespace Newsletter.Consumer
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                GroupId = "Newsletter",
+                BootstrapServers = "localhost:9092"
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+            consumer.Subscribe("New_Request");
+
+            while (!false)
+            {
+                var consumeResult = consumer.Consume();
+                Log.Information($"Consumed message '{consumeResult.Message.Value}' from the topic:'{consumeResult.Topic}' in the partition:'{consumeResult.Partition}' and offset:'{consumeResult.Offset}' ");
+            }
+
+            consumer.Close();
+
+            try
+            {
+                while (true)
+                {
+                    try
+                    {
+                        var consumeResult = consumer.Consume();
+                        Log.Information($"Mensagem consumida é '{consumeResult.Message.Value}' do Topico:'{consumeResult.Topic}' na partition:'{consumeResult.Partition}' e offset:'{consumeResult.Offset}' ");
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Log.Error($"Error occured: {e.Error.Reason}");
+                    }
+                }
+
+            }
+
+            catch (OperationCanceledException)
+            {
+                consumer.Close();
+            }
+        }
+    }
+}
---
+++ Newsletter.Producer/Program.cs
@@ -0,0 +1,29 @@
+﻿using Confluent.Kafka;
+using Serilog;
+using System;
+using System.Threading.Tasks;
+
+namespace Newsletter.Producer
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    var newRequest = await producer.ProduceAsync("New_Request", new Message<Null, string> { Value = "Request Newsletter" });
+                    Log.Information($"Delivery '{newRequest.Value}' for topic:'{newRequest.Topic}' partition:'{newRequest.Partition}' e offset:'{newRequest.Offset}' ");
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Log.Information($"Delivery Fail: {e.Error.Reason}");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
