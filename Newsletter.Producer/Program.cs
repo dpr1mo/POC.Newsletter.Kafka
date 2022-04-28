392aa5da0a37765f535f97d66fbc7c19135c6272
Danielly Primo <60229135+dan-primo@users.noreply.github.com>
2022-04-27 23:39:33 -0300

    !refactor: New changes consumer and producer

--- Newsletter.Producer/Program.cs
+++ Newsletter.Producer/Program.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace Newsletter.Producer
 {
-    class Program
+    internal class Program
     {
         public static async Task Main(string[] args)
         {
@@ -15,8 +15,8 @@ namespace Newsletter.Producer
             {
                 try
                 {
-                    var newRequest = await producer.ProduceAsync("New_Request", new Message<Null, string> { Value = "Request Newsletter" });
-                    Log.Information($"Delivery '{newRequest.Value}' for topic:'{newRequest.Topic}' partition:'{newRequest.Partition}' e offset:'{newRequest.Offset}' ");
+                    var newEmailRegistration = await producer.ProduceAsync("new-registration-newsletter", new Message<Null, string> { Value = "poc@kafka.com" });
+                    Log.Information($"Delivery '{newEmailRegistration.Value}' for topic:'{newEmailRegistration.Topic}' partition:'{newEmailRegistration.Partition}' e offset:'{newEmailRegistration.Offset}' ");
                 }
                 catch (ProduceException<Null, string> e)
                 {
